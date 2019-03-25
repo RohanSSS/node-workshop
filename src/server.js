7d05966ff1a7b1643db24d24c73ebaa59ffd14a0
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-03-25 14:26:00 +0000

    added fs, called index.html

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 var http = require("http");
+var fs = require("fs");
 
 var message = "I am so happy - on it like a car bonnet";
 
 function handler(request, response) {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(message);
-  response.end();
+  var method = request.method;
+  console.log(method);
+  var endpoint = request.url;
+  console.log(endpoint);
+  if (endpoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/node") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("Bob's your node");
+    response.end();
+  } else if (endpoint === "/girls") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("Bob's your girls");
+    response.end();
+  }
 }
 
 var server = http.createServer(handler);
