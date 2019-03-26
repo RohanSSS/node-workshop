baa69fed7cc148af620385a1ee347dc500b57d31
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-03-26 11:44:04 +0000

    POSTING THINGS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var fs = require("fs");
+var querystring = require("querystring");
 
 var message = "I am so happy - on it like a car bonnet";
 
@@ -16,32 +17,48 @@ function handler(request, response) {
   console.log(method);
   var endpoint = request.url;
   console.log(endpoint);
-  if (endpoint === "/") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile(__dirname + "/../public/index.html", function(error, file) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      response.end(file);
+
+  if (method === "GET") {
+    if (endpoint === "/") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      fs.readFile(__dirname + "/../public/index.html", function(error, file) {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        response.end(file);
+      });
+    } else if (endpoint === "/node") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("Bob's your node");
+      response.end();
+    } else if (endpoint === "/girls") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("Bob's your girls");
+      response.end();
+    } else {
+      var extension = endpoint.split(".")[1];
+      fs.readFile(__dirname + "/../public" + endpoint, function(error, file) {
+        if (error) {
+          console.log("this is the else");
+          console.log(error);
+          return;
+        }
+        response.writeHead(200, { "Content-Type": extensionType[extension] });
+        response.end(file);
+      });
+    }
+  } else if (method === "POST") {
+    var allTheData = "";
+    request.on("data", function(chunkOfData) {
+      allTheData += chunkOfData;
     });
-  } else if (endpoint === "/node") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("Bob's your node");
-    response.end();
-  } else if (endpoint === "/girls") {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("Bob's your girls");
-    response.end();
-  } else {
-    var extension = endpoint.split(".")[1];
-    fs.readFile(__dirname + "/../public" + endpoint, function(error, file) {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      response.writeHead(200, { "Content-Type": extensionType[extension] });
-      response.end(file);
+
+    request.on("end", function() {
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.writeHead(302, { Location: "/" });
+      response.end();
     });
   }
 }
